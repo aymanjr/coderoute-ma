bf645b29e85a7b91ff2d3f9a3d14c60dea04b56d
ayman jrm <ayman.jarmoune@outlook.fr>
2022-08-13 18:29:13 +0100

    changes

---
+++ app/Models/Question.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = ['title','image','reponse1','reponse2','reponse3','reponse4','answer'];
+}
