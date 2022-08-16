ebc7f9961693aeaaf02787945a36ec29e3a95e7b
MAYCHINE MOHAMED <31590745+MedMaychin@users.noreply.github.com>
2022-08-16 19:45:52 +0100

    courses

---
+++ resources/views/pages/courses/course.blade.php
@@ -0,0 +1,159 @@
+@extends('layout')
+@section('content')
+    <div class="container">
+        <div class="text-center py-5">
+            <div class="row">
+                <div class="col">
+                    <img src="" alt="image1">
+                </div>
+                <div class="col">
+                    تعليم السياقة بالمغرب
+                </div>
+                <div class="col">
+                    <img src="" alt="image2">
+                </div>
+            </div>
+        </div>
+
+        <div class="album py-5 bg-light">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="" data-holder-rendered="true">
+                            <div class="card-body">
+                                <p class="card-text text-right">ملخص الدروس النظرية</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-warning">View</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="" data-holder-rendered="true">
+                            <div class="card-body">
+                                <p class="card-text text-right">ملخص الدروس النضرية</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-warning">View</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="" data-holder-rendered="true">
+                            <div class="card-body">
+                                <p class="card-text text-right">ملخص الدروس النضرية</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-warning">View</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="" data-holder-rendered="true">
+                            <div class="card-body">
+                                <p class="card-text text-right">ملخص الدروس النضرية</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-warning">View</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="" data-holder-rendered="true">
+                            <div class="card-body">
+                                <p class="card-text text-right">ملخص الدروس النضرية</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-warning">View</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="" data-holder-rendered="true">
+                            <div class="card-body">
+                                <p class="card-text text-right">ملخص الدروس النضرية</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-warning">View</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="" data-holder-rendered="true">
+                            <div class="card-body">
+                                <p class="card-text text-right">ملخص الدروس النضرية</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-warning">View</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="" data-holder-rendered="true">
+                            <div class="card-body">
+                                <p class="card-text text-right">ملخص الدروس النضرية</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-warning ">View</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="" data-holder-rendered="true">
+                            <div class="card-body">
+                                <p class="card-text text-right">ملخص الدروس النضرية</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-warning">View</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
