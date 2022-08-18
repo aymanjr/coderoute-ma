a4d9ce34d633721475ef96e522dc2d871dbc9c86
MAYCHINE MOHAMED <31590745+MedMaychin@users.noreply.github.com>
2022-08-18 16:02:52 +0100

    change something
    nice

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,18 +1,22 @@
 @extends('layout')
 
 @section('content')
-    <div class="container">
+    <div class="container py-5">
         <div class="row">
             <div class="col-md-4">
                 <h4 class="text-center"><strong>Test Code De route</strong></h4>
                 <hr>
                 <a href="/quiz">
-                    <div class="profile-card-2"><img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-2.jpg"
+                    <div class="profile-card-2">
+                        <img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-2.jpg"
                             class="img img-responsive">
                         <div class="profile-name">Start Maintenant</div>
                         <div class="profile-username">40 Questions</div>
-                        <div class="profile-icons"><a href=""><i class="fa fa-facebook"></i></a><a href="#"><i
-                                    class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+                        <div class="profile-icons"><a href="">
+                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-linkedin"></i></i></a>
+                        </div>
                     </div>
                 </a>
             </div>
