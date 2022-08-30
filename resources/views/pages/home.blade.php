09b655adf2913154f8f474d00494bea35944335e
ayman jrm <ayman.jarmoune@outlook.fr>
2022-08-30 19:18:11 +0100

    added the courses to the home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -20,6 +20,23 @@
                     </div>
                 </a>
             </div>
+            <div class="col-md-4">
+                <h4 class="text-center"><strong>Cours de Code De Route</strong></h4>
+                <hr>
+                <a href="/courses">
+                    <div class="profile-card-2">
+                        <img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-2.jpg"
+                            class="img img-responsive">
+                        <div class="profile-name">Les Cours</div>
+                        <div class="profile-username">Des Cours detailer</div>
+                        <div class="profile-icons"><a href="">
+                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-linkedin"></i></i></a>
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 @endsection
